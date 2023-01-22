fc8ee5a4bcda0608f69d29cdfee2792eccadd6a6
SCNomeD <vladiiimir1991@gmail.com>
2023-01-22 17:31:04 +0300

    Module_34.8.1

--- HomeApi/Controllers/DevicesController.cs
+++ HomeApi/Controllers/DevicesController.cs
@@ -45,7 +45,22 @@ namespace HomeApi.Controllers
             return StatusCode(200, resp);
         }
 
-        // TODO: Задание: напишите запрос на удаление устройства
+        /// <summary>
+        /// Удаление устройства
+        /// </summary>
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> Delete(
+            [FromRoute] Guid id)
+        {
+            var device = await _devices.GetDeviceById(id);
+            if (device == null)
+                return StatusCode(400, $"Ошибка: Устройство с идентификатором {id} не существует.");
+
+            await _devices.DeleteDevice(device);
+
+            return StatusCode(200, $"Устройство было удалено!");
+        }
 
         /// <summary>
         /// Добавление нового устройства
